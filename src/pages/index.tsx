6aea29cec466309807b29cbfb2e0b9c01cb6f0b3
vladyslavpodenko <163551550+vladyslavpodenko@users.noreply.github.com>
2024-05-13 02:26:13 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
                         e.preventDefault();
                         // @ts-ignore
                         document.getElementById('development-packages').scrollIntoView({behavior: 'smooth'});
-                    }} className="button">Get an early access</a>
+                    }} <a href="https://jbd22dr7xmp.typeform.com/to/kT5IuWDP" className="button">Get an early access</a>
                     <a href="https://calendly.com/vladsolop/15min" className="button__book">Book a demo {">"}</a>
                 </div>
             </header>
