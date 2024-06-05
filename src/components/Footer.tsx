a2a4170074782b7be870cbbe6364b4539c084ef7
vladyslavpodenko <163551550+vladyslavpodenko@users.noreply.github.com>
2024-06-05 21:55:25 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ const Footer=()=>{
     return (
         <footer className="site-footer">
             <div className="container">
-                <p><a href="gmail:spatium@spatium.com" className="footer-email">Contact vladyslavsolop@gmail.com</a></p>
-                <p>&copy; All Rights Not Reserved.</p>
+                <p><a href="gmail:spatium@spatium.com" className="footer-email">Contact vladpodenko01@gmail.com</a></p>
+                <p>&copy; All Rights Reserved.</p>
             </div>
         </footer>
     )
